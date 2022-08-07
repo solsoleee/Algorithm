8c341119253b7caaa5e4493736a172084f908cea
hasoleee <wsw3727@gmail.com>
2022-08-07 21:29:20 +0900

    codeup end

--- 6094.py
+++ 6094.py
@@ -1,12 +1,12 @@
-n=int(input())
-a = input().split() 
+n = int(input())
+a = input().split()
 
-for i in range(n) : 
+for i in range(n) :
   a[i] = int(a[i])
 
-min=0
+min = a[0]
+for i in range(0, n) :
+  if a[i] < min :
+    min = a[i]
 
-for i in range(n-1) : 
-  if a[i]<a[i+1]:
-      min=a[i]
 print(min)
