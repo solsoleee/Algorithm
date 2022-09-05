a69ff42e6743ad47e27dcb0b3337151e5a0af34d
hasoleee <wsw3727@gmail.com>
2022-09-05 19:10:57 +0900

    commit

--- backjoon/2798.py
+++ backjoon/2798.py
@@ -1,9 +1,10 @@
-sum=0
+result=0
 a,b=map(int,input().split())
-n=list(input().split())
+n=list(map(int,input().split()))
 for i in range(a):
-    n[i]=int(n[i])
-    sum+=n[i]
-    if sum>=b:
-        break
-print(sum)
+    for j in range (i+1,a):
+        for k in range(j+1,a):
+            sum=n[i]+n[j]+n[k]
+            if sum<=b:
+                result=max(result,sum)
+print(result)
