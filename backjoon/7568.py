4a8783d2dd8dcbbf947bcd6883b1cadb3e838bca
hasoleee <wsw3727@gmail.com>
2022-09-08 15:46:39 +0900

    commit

--- backjoon/7568.py
+++ backjoon/7568.py
@@ -1,5 +1,11 @@
 n=int(input())
+data_list=[]
 for i in range(n):
-    a,b=list(map(int,input().split()))
-    if (a[i]<a[i+1] and b[i]<b[i+1]):
-        rank=1
+    a,b=map(int,input().split())
+    data_list.append((a,b))
+for i in range(n):
+    rank=1
+    for j in range(n):
+        if(data_list[i][0]<data_list[j][0] and data_list[i][1]<data_list[j][1]):
+            rank+=1
+    print(rank,end=' ')
