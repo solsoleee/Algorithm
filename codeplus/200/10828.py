ff7167821070186cf15d36d808844a250aed25dc
hasoleee <wsw3727@gmail.com>
2022-09-15 13:05:32 +0900

    study

--- codeplus/200/10828.py
+++ codeplus/200/10828.py
@@ -1,3 +1,32 @@
-n=int(input())
+from inspect import stack
+import sys
+n=int(sys.stdin.readline())
+stack=[]
 for i in range(n):
+    word=sys.stdin.readline().split()
+    order=word[0]
     
+    if(order=="push"):
+        value=word[1]
+        stack.append(value)
+    
+    elif (order=="pop"):
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack.pop())
+    
+    elif (order=="size"):
+        print(len(stack))
+    
+    elif (order=="empty"):
+        if len(stack)==0:
+            print(1)
+        else:
+            print(0)
+    
+    elif (order=="top"):
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack[-1])
