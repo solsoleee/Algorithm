6294bb48bff9e080d2a6e2ef8cb969ccfced2707
hasoleee <wsw3727@gmail.com>
2022-09-28 23:34:06 +0900

    commit

--- codeplus/200/10845.py
+++ codeplus/200/10845.py
@@ -6,21 +6,21 @@ for i in range(N):
     if(cmd[0]=='push'):
         queue.append(cmd[1])
     elif(cmd[0]=='front'):
-        print(queue[-1])
         if(len(queue)==0):
             print("-1")
+        else: print(queue[0])
     elif(cmd[0]=='back'):
-        print(queue[1])
         if(len(queue)==0):
             print("-1")
+        else: print(queue[-1])
     elif(cmd[0]=='empty'):
         if(len(queue)==0):
-            print("-1")
+            print("1")
         else:
             print("0")
     elif(cmd[0]=='size'):
         print(len(queue))
     elif(cmd[0]=='pop'):
-        print(queue.pop(cmd[1]))
         if(len(queue)==0):
             print("-1")
+        else: print(queue.pop(0))
