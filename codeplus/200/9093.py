332f97781810cc743c3acd78e844762f9588561e
hasoleee <wsw3727@gmail.com>
2022-11-10 09:33:45 +0900

    study

--- codeplus/200/9093.py
+++
@@ -1,8 +0,0 @@
-from inspect import stack
-import sys
-N=int(sys.stdin.readline())
-stack=[]
-for _ in range (N):
-    word=sys.stdin.readline().split()
-    for i in word:
-        print(i[::-1],end=' ')
