9f444003055355eca47c14c23579d7f337210986
solsoleee <wsw3727@gmail.com>
2024-01-25 18:41:30 +0900

    study

--- baeckjoon/1012.py
+++ baeckjoon/1012.py
@@ -1,30 +1,36 @@
+import sys
+sys.setrecursionlimit(10000)
 
-def dfs(x,y,arr):
-    if x >= b or x-1<0 or y>=a or y-1<0:
-        return False
-    if arr[x][y] == 1:
-        print("whq")
-        arr[x][y] = 0
-        dfs(x-1,y)
-        dfs(x,y-1)
-        dfs(x+1,y)
-        dfs(x,y+1)
-        return True
-
-arr=[]
-t=int(input())
+t=int(sys.stdin.readline())
 for i in range(t):
-    a,b,c=map(int,input().split())
-    
-    arr=[[0]*(a+1) for i in range(b+1)]
-    result=0
-    for j in range(c):
-        x,y=map(int,input().split())
+    m,n,k=map(int,sys.stdin.readline().split())
+
+    arr=[[0]*(m) for _ in range(n)]
+
+    for i in range(k):
+        x,y=map(int,sys.stdin.readline().split())
         arr[y][x]=1
-    print(arr)
-    if dfs(0,0,arr):
-        print("ddd")
-        result+=1
-print(result)
-        
-        
+
+    def dfs(x,y):
+        if x<0 or y<0 or x>=n or y>=m:
+            return False
+        if arr[x][y]==1:
+            arr[x][y]=0
+            dfs(x-1,y)
+            dfs(x,y-1)
+            dfs(x+1,y)
+            dfs(x,y+1)
+            return True
+        return False
+
+    result=0
+
+    for i in range(n):
+        for j in range(m):
+            if dfs(i,j)==True:
+                result+=1
+
+    print(result)
+
+    
+
