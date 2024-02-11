1f8799cc44d10ed776ddcf7cbf233b46c2774869
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-02-11 18:44:03 +0900

    [Silver IV] Title: 국영수, Time: 480 ms, Memory: 148148 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,12 @@
+n=int(input())
+arr=[]
+
+
+for i in range(n):
+    name, a, b, c = input().split()
+    arr.append([name, int(a), int(b), int(c)])
+    
+arr.sort(key=lambda x: (-x[1], x[2], -x[3], x[0]))
+
+for i in range(n):
+    print(arr[i][0])
