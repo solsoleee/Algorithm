8b136f308c8f94cd05cb0a05eaf3b33f139a8c9b
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-02-22 22:39:28 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,7 +1,7 @@
 -- 코드를 입력하세요
-SELECT a.ANIMAL_ID, a.name
-from ANIMAL_OUTS as a
-left join ANIMAL_ins as b
-on a.ANIMAL_ID=b.ANIMAL_ID
-where b.ANIMAL_ID is null
-order by a.animal_id, a.name
+SELECT b.ANIMAL_ID,	b.NAME
+from ANIMAL_INS as a
+right join animal_outs as b
+on a.animal_id=b.animal_id
+where a.animal_id is null
+order by b.animal_id, b.name
