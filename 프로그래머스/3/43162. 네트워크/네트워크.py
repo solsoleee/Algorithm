c6ca5d5dca2b782795d926f5b3bd2cb707fe14de
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-02-22 13:09:15 +0900

    [level 3] Title: 네트워크, Time: 0.90 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,23 +1,27 @@
 from collections import deque
 
 def solution(n, computers):
-    visited=[0]*(n+1)
-    def bfs(v, visited) :
-        que=deque([v])
-        visited[v]=1
-        while que:
-            v=que.popleft()
-            for i in range(n):
-                if visited[i]==0 and computers[v][i]==1:
-                    que.append(i)
-                    visited[i]=1
-        
-        return False
     answer = 0
+    visited=[0]*(n+1)
+    
+    def bfs(x):
+        que=deque([x])
+        while que:
+            x=que.popleft()
+            visited[x]=1
+            for i in range(n):
+                if computers[x][i]==1 and visited[i]==0:
+                    visited[i]=1
+                    que.append(i)
+                    print(i)
+        return False
+                    
     for i in range(n):
         for j in range(n):
-            if visited[j]==0 and computers[i][j]==1:
-                bfs(j, visited)
+            if computers[i][j]==1 and visited[j]==0:
+                bfs(j)
                 answer+=1
+            
+        
     
     return answer
