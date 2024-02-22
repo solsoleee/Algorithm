1a1335904e2797571c212ca867edb6ac06ab0f04
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-02-22 11:34:15 +0900

    [level 2] Title: 타겟 넘버, Time: 1.45 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,18 +1,22 @@
+from collections import deque
+
 def solution(numbers, target):
     
-    res=[0] # 모든계산결과
-    answer = 0 # 개수
-    
-    for num in numbers:
-        temp=[]
+    def bfs(numsers, target):
+        cnt=0
+        now=[0] #모든 계산 결과
+        for num in numbers: #numbers를 다 돌려줌
+            tmp=[]
+            for parent in now: #모든 계산 결과도 돌려줌
+                tmp.append(parent+num) #현재 계산에서 새로운 값을 더하거나 뺀 것도 추가해줌
+                tmp.append(parent-num)
+            
+            now=tmp #새로운 값으로 계속 update
         
-        for i in res:        
-            temp.append(i+num) #현재 계산 결과
-            temp.append(i-num)
-        
-        res=temp
-    for r in res:
-        if r==target:
-            answer+=1
-    
+        for n in now:
+            if n==target:
+                cnt+=1
+        return cnt
+    answer = bfs(numbers, target)
     return answer
+        
