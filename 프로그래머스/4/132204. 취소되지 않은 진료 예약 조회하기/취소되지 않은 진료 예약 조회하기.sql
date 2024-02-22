c6445de1989b3dc0bd7ebfad6c39cf4b1bfde5ab
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-02-22 16:50:55 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,11 +1,12 @@
--- 코드를 입력하세요
-SELECT a.APNT_NO, c.PT_NAME,	a.PT_NO,	a.MCDP_CD,	b.DR_NAME,	a.APNT_YMD
-from APPOINTMENT as a
-join DOCTOR as b
-on b.DR_ID = a.MDDR_ID
-join PATIENT as c
-on a.PT_NO=c.PT_NO
-where date_format(a.APNT_YMD,'%Y-%m-%d')='2022-04-13' and a.MCDP_CD='CS' and APNT_CNCL_YN='N'
-order by a.APNT_YMD 
+with appointment as (
+    select a.APNT_NO, c.MCDP_CD, c.DR_NAME, a.APNT_YMD, a.PT_NO
+    from APPOINTMENT as a
+    inner join DOCTOR as c on a.MDDR_ID=c.DR_ID
+    where c.mcdp_cd='CS' and a.APNT_CNCL_YN='N' and APNT_CNCL_YMD is null
+    and a.APNT_YMD like '2022-04-13%'
+)
 
-# 
+select a.APNT_NO, p.PT_NAME, p.PT_NO, a.MCDP_CD, a.DR_NAME, a.APNT_YMD
+from appointment as a
+inner join PATIENT as p on a.PT_NO=p.PT_NO
+order by APNT_YMD
