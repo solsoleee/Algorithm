93a3bc2757510fd4a724ab5b5ea4f45f7f847828
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-02-23 21:25:40 +0900

    [level 1] Title: 최소직사각형, Time: 3.52 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,16 +1,13 @@
 def solution(sizes):
-    n=len(sizes)
-    m=2
     
-    for i in range(n):
-        if sizes[i][0] > sizes[i][1]:
+    answer = 0
+    for i in range(len(sizes)):
+        if sizes[i][0]<sizes[i][1]:
             sizes[i][0], sizes[i][1] = sizes[i][1], sizes[i][0]
-
+    
     max_value=0
-    for j in range(n):
-        max_value=max(sizes[j][1], max_value)
+    for s in sizes:
+        max_value=max(max_value, s[1])
 
-    
-    
-    answer = max(sizes)[0]*max_value
+    answer = max_value * max(sizes)[0]
     return answer
