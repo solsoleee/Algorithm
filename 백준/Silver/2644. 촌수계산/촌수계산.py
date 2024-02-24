b65d1cba8978bfe39787a1d9ecde93599f5d5ee1
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-02-24 11:59:37 +0900

    [Silver II] Title: 촌수계산, Time: 140 ms, Memory: 113164 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+n=int(input())
+a,b=map(int, input().split())
+
+graph=[[0]*(n+1) for i in range(n+1)]
+
+
+m=int(input())
+for i in range(m):
+    x,y=map(int, input().split())
+    graph[x][y]=1
+    graph[y][x]=1
+
+visited=[0]*(n+1)
+
+def bfs(start, target):
+    que=deque()
+    que.append((start, 0))
+    visited[start]=1
+    while que:
+        x, index=que.popleft()
+        if target==x:
+            return index
+        for i in range(1, n+1):
+            if visited[i]==0 and graph[x][i]==1:
+                que.append((i, index+1))
+                visited[i]=1
+    return -1
+
+print(bfs(a,b))
