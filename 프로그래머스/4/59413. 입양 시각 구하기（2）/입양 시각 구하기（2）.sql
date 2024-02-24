95810e56f6d0a9bdcd82aa69143ae2647c1b3078
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-02-24 13:46:06 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,7 +1,9 @@
-SET @HOUR = -1;
-SELECT (@HOUR := @HOUR +1) AS HOUR,
-    (SELECT COUNT(HOUR(DATETIME)) 
-    FROM ANIMAL_OUTS 
-    WHERE HOUR(DATETIME)=@HOUR) AS COUNT 
-    FROM ANIMAL_OUTS
-WHERE @HOUR < 23;
+set @hour = -1;
+select (@hour:=@hour+1) as hour,
+( select count(hour(datetime))
+    from ANIMAL_OUTS
+    where hour(datetime)=@hour
+) as count
+from ANIMAL_OUTS
+where @hour<23
+order by hour
