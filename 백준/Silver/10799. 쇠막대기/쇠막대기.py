6bb88250468e2638a0ce63996d6ee41291da73fc
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-03-21 22:09:05 +0900

    [Silver II] Title: 쇠막대기, Time: 128 ms, Memory: 114520 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -1,16 +1,20 @@
-a=input()
-b=[]
-cnt=0
-pre=0
-for i in a:
-    if i=="(":
-        b.append(i)
-    elif i==")" and pre=="(":
-        b.pop()
-        cnt+=len(b)
-    else:
-        b.pop()
-        cnt+=1
-    pre=i
+def solution(arr):
+    answer=0
+    stack=[]
+    pre=0
+    for i in arr:
+        if i == "(":
+            stack.append(i)
+        elif i ==")" and pre=="(": #지금 나오는 문자가 )이면서 이전 문자가 (이라면 레이저
+            stack.pop()
+            answer+=len(stack)
+        elif i==")": #막대기 끝
+            stack.pop()
+            answer+=1
+        pre=i
     
-print(cnt)
+    return answer
+
+if __name__ == "__main__":
+    arr=input()
+    print(solution(arr))
