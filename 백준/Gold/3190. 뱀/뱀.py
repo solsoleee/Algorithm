913a8408f290a497195d8a82f9d7d6f49903ddd5
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-03-23 22:57:30 +0900

    [Gold IV] Title: 뱀, Time: 144 ms, Memory: 113164 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,58 +1,59 @@
 from collections import deque
 
-n = int(input())
-graph = [[0] * n for _ in range(n)]
+n=int(input())
+board=[[0]*n for _ in range(n)]
 
-k = int(input())
-for _ in range(k):
-    a, b = map(int, input().split())
-    graph[a - 1][b - 1] = 2
+k=int(input())
+for i in range(k):
+    a,b=map(int, input().split())
+    board[a-1][b-1]=2
 
-l = int(input())
-turn_dic = dict()
-for _ in range(l):
-    x, c = input().split()
-    turn_dic[int(x)] = c
+turn_dict=dict()
 
-# 동 남 서 북
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
+l=int(input())
+for i in range(l):
+    x,c=input().split()
+    turn_dict[int(x)]=c
 
-def turn(d):
+x,y=0,0 #뱀 초기 위치
+direction=0 #현재 방향
+cnt=0 #시간
+#동, 남, 서, 북
+dx=[0,1,0,-1]
+dy=[1,0,-1,0]
+
+def turn(c):
     global direction
-    if d == "L":
-        direction = (direction - 1) % 4
+    if c=="L":
+        direction=(direction-1)%4
     else:
-        direction = (direction + 1) % 4
+        direction=(direction+1)%4
 
-cnt = 0
-direction = 0
-x, y = 0, 0
-que = deque([[x, y]]) # 뱀의 위치 초기화 수정
-graph[x][y] = 1  # 뱀의 초기 위치를 1로 설정
+que=deque()
+que.append((x,y))
+board[x][y]=1 #뱀 처음 위치
 
 while True:
-    nx = x + dx[direction]
-    ny = y + dy[direction]
-    cnt += 1  # 시간(이동 횟수) 증가
-    
-    if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] != 1:
-        # 사과가 있을 때
-        if graph[nx][ny] == 2:
-            graph[nx][ny] = 1
-            que.append([nx, ny])
-        # 사과가 없을 때
-        else:
-            graph[nx][ny] = 1
-            que.append([nx, ny])
-            tx, ty = que.popleft() #꼬리 부분 제거 
-            graph[tx][ty] = 0
-        
-        x, y = nx, ny  # 다음 위치로 머리 이동
-        
-        if cnt in turn_dic:  # 회전 체크
-            turn(turn_dic[cnt])
+    nx = x+dx[direction]
+    ny = y+dy[direction]
+
+    cnt+=1
+    if nx>=0 and nx<n and ny>=0 and ny<n and board[nx][ny]!=1:
+        if board[nx][ny]==2: #사과가 있다면
+            board[nx][ny]=1
+            que.append((nx, ny))
+        else: #사과가 없다면
+            board[nx][ny]=1
+            que.append((nx, ny))
+
+            #꼬리제거
+            qx, qy = que.popleft()
+            board[qx][qy]=0
+            
+        if cnt in turn_dict:
+            turn(turn_dict[cnt])
+        x,y = nx, ny
     else:
-        break  # 벽이나 자기 자신의 몸과 부딪혔을 때
+        break
 
 print(cnt)
