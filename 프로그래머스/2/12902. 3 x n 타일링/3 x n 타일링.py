7c9335d42568e2a7e6dc5299114f656af5c1c411
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-03-23 18:08:54 +0900

    [level 2] Title: 3 x n 타일링, Time: 528.84 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.py
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.py
@@ -1,17 +1,14 @@
 def solution(n):
-    
+
     mod=1000000007
     dp=[0]*(n+1)
-    #홀수인 경우
-    if n%2!=0:
-        return 0
-    
     dp[2]=3
     dp[4]=11
-    
+    if n%2!=0:
+        return 0
     for i in range(6, n+1, 2):
-        dp[i]=dp[i-2]*3 +2
-        for j in range(i-4, -1,-2):
-            dp[i]+=dp[j]*2
+        dp[i]=dp[i-2]*3+2    
+        for j in range(i,-1,-2):
+            dp[i]+=dp[j-4]*2
     dp[n]=dp[n]%mod
     return dp[n]
