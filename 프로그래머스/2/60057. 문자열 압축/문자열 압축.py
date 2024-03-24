0331a9e6c5bfe7643d259b811ea30c571529f5c9
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-03-24 11:00:17 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,25 +1,18 @@
 def solution(s):
-    answer=[]
-
-    if len(s)==1:
-        return 1
-    
-    for i in range(1, len(s)+1):
-        tmp=s[:i]
-        result=''
-        cnt=1
+    answer = []
+    cnt=1
+    for i in range(1, len(s)+1): #i개씩 자름
+        result='' #문자열 저장
+        tmp=s[:i] #이전 문자열
         for j in range(i, len(s)+i, i):
-            #이전거랑 같다면
-            if tmp==s[j:j+i]:
+            if tmp==s[j:i+j]:
                 cnt+=1
-            else: #이전이랑 같이 않을 때
-                if cnt>=2: #문자열 압축
-                    result+=str(cnt)+tmp
-                else: #이전거랑 같지도 않고 압축도 안됨
-                    result+=tmp
-                tmp=s[j:j+i] #이전거로
+            else:
+                if cnt!=1: #문자열이 같을 때 (압축해야함)
+                    result=result+str(cnt)+tmp
+                else: #압축 안될 때 그냥 더함
+                    result=result+tmp
+                tmp=s[j:i+j]
                 cnt=1
         answer.append(len(result))
-                    
-            
     return min(answer)
