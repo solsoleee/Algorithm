5e1ac0eaedae0bf5735233c02dc23a6d15c00b02
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-04-11 21:36:19 +0900

    [level 3] Title: 기둥과 보 설치, Time: 1551.55 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
@@ -1,14 +1,13 @@
-def check_valid(answer):
-    for x, y, a in answer:
-        if a == 0:  # 기둥인 경우
-            # 바닥 위이거나 보의 한쪽 끝 부분 위이거나 다른 기둥 위인 경우
-            if y == 0 or [x-1, y, 1] in answer or [x, y, 1] in answer or [x, y-1, 0] in answer:
+def check(answer):
+    for x,y,a in answer:
+
+        if a==0: #기둥이면
+            if y==0 or [x,y-1,0] in answer or [x-1,y,1] in answer or [x,y,1] in answer:
                 continue
             else:
                 return False
-        elif a == 1:  # 보인 경우
-            # 한쪽 끝 부분이 기둥 위에 있거나 양쪽 끝 부분이 다른 보와 동시에 연결되어 있는 경우
-            if [x, y-1, 0] in answer or [x+1, y-1, 0] in answer or ([x-1, y, 1] in answer and [x+1, y, 1] in answer):
+        elif a==1: #보라면
+            if [x,y-1,0] in answer or [x+1,y-1,0] in answer or ([x-1,y,1] in answer and [x+1,y,1] in answer):
                 continue
             else:
                 return False
@@ -16,15 +15,17 @@ def check_valid(answer):
 
 def solution(n, build_frame):
     answer = []
+    
     for frame in build_frame:
-        x, y, a, b = frame
-        if b == 1:  # 설치하는 경우
-            answer.append([x, y, a])
-            if not check_valid(answer):
-                answer.remove([x, y, a])
-        else:  # 삭제하는 경우
-            answer.remove([x, y, a])
-            if not check_valid(answer):
-                answer.append([x, y, a])
-    answer.sort()
+        x,y,a,b=frame
+        if b==1: #설치
+            answer.append([x,y,a])
+            if not check(answer):
+                answer.remove([x,y,a])
+        else: #삭제
+            answer.remove([x,y,a])
+            if not check(answer):
+                answer.append([x,y,a])
+        answer.sort()
+    print(answer)
     return answer
