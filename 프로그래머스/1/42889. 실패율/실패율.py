de2d17ee61e9ce53a9038a8d24266f84c159962b
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-05-16 22:29:44 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,17 +1,21 @@
 def solution(N, stages):
-    answer=[]
-    #스테이지에 도달한 수 전체
-    #스테이지에 도달하지 못한 수 (그 스테이지에 멈춘 수)
-    length=len(stages)
-    for i in range(1,N+1):
-        #1번 스테이지 실패율
-        s=stages.count(i) #스테이지에 멈춤 
-        if length==0:
-            res=0
+    answer = []
+    
+    stage_count = [0]*(N+2)
+    
+    for s in stages:
+        stage_count[s]+=1 #도달한 플레이어 하지만 클리어를 하지 못한
+    
+    reached = len(stages)
+    
+    for i in range(1, N+1):
+        if reached == 0:
+            fail=0
         else:
-            res=s/length
-        answer.append((i, res))
-        length-=s
-    answer.sort(key=lambda x:(x[1]), reverse=True)
-    answer=[i[0] for i in answer]
-    return answer
+            fail=stage_count[i]/reached
+        answer.append((i, fail))
+        reached-=stage_count[i]
+    answer.sort(key=lambda x:(-x[1],x[0]))
+    res=[answer[i][0] for i in range(N)]
+    print(res)
+    return res
