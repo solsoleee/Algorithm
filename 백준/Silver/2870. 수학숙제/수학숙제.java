c6e09d6fb1e6e73c9bc1bae8bcab45e7b6389a0d
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-07-17 22:52:44 +0900

    [Silver IV] Title: 수학숙제, Time: 212 ms, Memory: 17096 KB -BaekjoonHub

---
+++ 백준/Silver/2870. 수학숙제/수학숙제.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    private static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    private static StringBuilder output = new StringBuilder();
+    private static StringTokenizer tokens;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int n = Integer.parseInt(input.readLine());
+        List<BigInteger> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            tokens = new StringTokenizer(input.readLine());
+            String pre = "";
+            Boolean flag = false; // 이전에 문자가 있었는지 여부
+            String str = tokens.nextToken();
+            for (int j = 0; j < str.length(); j++) {
+                if (Character.isDigit(str.charAt(j))) {
+                    flag = true;
+                    pre = pre + str.charAt(j);
+                } else {
+                    if (flag && !pre.isEmpty()) {
+                        list.add(new BigInteger(pre));
+                        pre = "";
+                        flag = false;
+                    }
+                }
+            }
+            if (flag && !pre.isEmpty()) {
+                list.add(new BigInteger(pre));
+            }
+        }
+
+        Collections.sort(list);
+        for (BigInteger r : list) {
+            System.out.println(r);
+        }
+    }
+}
