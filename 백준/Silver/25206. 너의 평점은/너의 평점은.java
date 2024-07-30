9cd71c5bcca51b2c5ca795f8809491bc16f7b40d
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-07-30 09:09:31 +0900

    [Silver V] Title: 너의 평점은, Time: 136 ms, Memory: 14624 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -1,54 +1,55 @@
-import java.util.*;
-
-
-
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	
-	public static double change(String grade) {
-	    if (grade.equals("A+")) {
-	        return 4.5;
-	    } else if (grade.equals("A0")) {
-	        return 4.0;
-	    } else if (grade.equals("B+")) {
-	        return 3.5;
-	    } else if (grade.equals("B0")) {
-	        return 3.0;
-	    } else if (grade.equals("C+")) {
-	        return 2.5;
-	    } else if (grade.equals("C0")) {
-	        return 2.0;
-	    } else if (grade.equals("D+")) {
-	        return 1.5;
-	    } else if (grade.equals("D0")) {
-	        return 1.0;
-	    } else if (grade.equals("F")) {
-	        return 0.0;
-	    } else {
-	        throw new IllegalArgumentException("Invalid grade: " + grade);
-	    }
-	}
+	private static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	private static StringBuilder output= new StringBuilder();
+	private static StringTokenizer tokens;
 	
-	public static void main(String[] args) {
+	
+	
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-        Scanner sc =new Scanner(System.in);
-        double total=0;
-        double num=0;
-        for(int i=0; i<20; i++) {
-        	String input[] = sc.nextLine().split(" ");
-        	String sub = input[0];
-        	double score = Double.parseDouble(input[1]);
-        	if (input[2].equals("P")) num+=0;
-        	else {
-        		double grade = change(input[2]);
-        		total+=(score * grade);
-        		num+=score;
-        	}
-        }
-        System.out.printf("%.6f",total/num);
-        sc.close();
-		
+		double grade=0;
+		double total=0;
+		for(int i=0; i<20; i++) {
+			tokens = new StringTokenizer(input.readLine());
+			String a = tokens.nextToken();
+			double b = Double.parseDouble(tokens.nextToken());
+			String c = tokens.nextToken();
+			if(!c.equals("P")) {
+				grade+=b;
+			}
+			
+			if(c.equals("A+")) {
+				total += b*4.5;
+			}
+			else if(c.equals("A0")) {
+				total += b*4.0;
+			}
+			else if(c.equals("B+")) {
+				total += b*3.5;
+			}
+			else if(c.equals("B0")) {
+				total += b*3.0;
+			}
+			else if(c.equals("C+")) {
+				total += b*2.5;
+			}
+			else if(c.equals("C0")) {
+				total += b*2.0;
+			}
+			else if(c.equals("D+")) {
+				total += b*1.5;
+			}
+			else if(c.equals("D0")) {
+				total += b*1.0;
+			}
+		}
+		System.out.printf("%.6f",total/grade);
 		
 	}
 
