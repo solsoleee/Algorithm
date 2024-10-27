a5a325c89be4e989102ee7287a0851e7b557471f
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-27 17:48:07 +0900

    [Gold IV] Title: 뱀, Time: 116 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -4,97 +4,80 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static StringTokenizer tokens;
-    static int n,k,l,d;
-    static int[][]map;
-    static boolean visited[][];
-    static Queue<int[]> que;
-    static Queue<int[]> back;
+    static int n, k, l, d;
+    static int[][] map;
+    static boolean[][] visited;
+    static Deque<int[]> snake; // 뱀의 몸통과 머리 위치를 함께 관리
     static Map<Integer, Character> direction;
-    static int[][] deltas = {{0,1}, {1,0},{0,-1},{-1,0}}; //+1하면 오른쪽 회전
+    static int[][] deltas = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // 방향 (우, 하, 좌, 상)
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
         n = Integer.parseInt(input.readLine());
-        map = new int [n][n];
+        map = new int[n][n];
         visited = new boolean[n][n];
         k = Integer.parseInt(input.readLine());
         direction = new HashMap<>();
-        for(int i=0; i<k; i++) {
-            tokens = new StringTokenizer(input.readLine());
-            int a = Integer.parseInt(tokens.nextToken()) -1;
-            int b = Integer.parseInt(tokens.nextToken()) -1;
-            map[a][b] = 1; //사과가 있다는 뜻
+        
+        for (int i = 0; i < k; i++) {
+            StringTokenizer tokens = new StringTokenizer(input.readLine());
+            int a = Integer.parseInt(tokens.nextToken()) - 1;
+            int b = Integer.parseInt(tokens.nextToken()) - 1;
+            map[a][b] = 1; // 사과 위치
         }
+        
         l = Integer.parseInt(input.readLine());
-        for(int i=0; i<l; i++) {
-            tokens = new StringTokenizer(input.readLine());
+        for (int i = 0; i < l; i++) {
+            StringTokenizer tokens = new StringTokenizer(input.readLine());
             int a = Integer.parseInt(tokens.nextToken());
             char b = tokens.nextToken().charAt(0);
-            direction.put(a,b);
+            direction.put(a, b);
         }
 
-        //뱀의 위치
-        que = new ArrayDeque<>();
-        back = new ArrayDeque<>();
-        que.offer(new int[] {0,0}); //뱀의 머리만
-        back.offer(new int[] {0,0}); //꼬리..
+        // 뱀 초기화
+        snake = new ArrayDeque<>();
+        snake.offerFirst(new int[]{0, 0}); // 뱀의 머리 위치
         visited[0][0] = true;
-        int len = 1; //뱀의 길이
-        int t = 0; //초
-
-        while(true) {
-
+        
+        int t = 0; // 시간
+        
+        while (true) {
             t++;
-            int loca[] = que.poll();
-            int x = loca[0]; //뱀의 머리
-            int y = loca[1];
+            int[] head = snake.peekFirst(); // 현재 뱀의 머리 위치
+            int nx = head[0] + deltas[d][0];
+            int ny = head[1] + deltas[d][1];
 
-            //벽이나 자기 자신의 몸과 부딪이면 끝
-            int nx = x + deltas[d][0];
-            int ny = y + deltas[d][1];
-            if(!check(nx,ny) || !bamCheck(nx,ny)) {
+            // 벽 또는 자기 자신과 충돌하면 게임 종료
+            if (!check(nx, ny) || visited[nx][ny]) {
                 break;
             }
 
-            //사과가 있는 경우, 없는 경우
-            if(map[nx][ny]==1) {
-                len ++;
-                map[nx][ny] =0; //사과없어짐
+            // 사과가 있는 경우, 없는 경우
+            if (map[nx][ny] == 1) {
+                map[nx][ny] = 0; // 사과 먹음
+            } else {
+                int[] tail = snake.pollLast(); // 꼬리 제거
+                visited[tail[0]][tail[1]] = false;
             }
-            //사과없을 때
-            else {
-                visited[x][y] = false;
-                back.poll();
-            }
+
+            // 뱀 머리를 새로운 위치에 추가하고 방문 처리
+            snake.offerFirst(new int[]{nx, ny});
             visited[nx][ny] = true;
-            back.offer(new int[] {nx,ny});
-            que.offer(new int [] {nx,ny});
-            //System.out.println(nx +" " + ny +" " +t);
 
-            //초에 해당하는 게 있으면
+            // 방향 변경 확인
             if (direction.containsKey(t)) {
-                if(direction.get(t) == 'D') turn(d,'D' );
-                else if(direction.get(t) == 'L') turn(d, 'L');
+                if (direction.get(t) == 'D') {
+                    d = (d + 1) % 4;
+                } else if (direction.get(t) == 'L') {
+                    d = (d + 3) % 4; // 왼쪽 회전
+                }
             }
         }
+
         System.out.println(t);
-
     }
 
-    //que에 뱀의 몸통과 부딪히는지 확인하는 함수
-    static boolean bamCheck(int x, int y) {
-
-        for(int b[] : back) {
-            if(b[0] == x && b[1] ==y) return false;
-        }
-        return true;
-    }
     static boolean check(int x, int y) {
-        return x>=0 && x<n && y>=0 && y<n;
-    }
-    static void turn(int di, char c) {
-        if(c == 'D') d = (di+1)%4;
-        else d = (di-1 +4)%4;
+        return x >= 0 && x < n && y >= 0 && y < n;
     }
 }
