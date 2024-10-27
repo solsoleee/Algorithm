993be1058b587fc3b90599f92e5f8ebfe3c7ca6f
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-27 23:12:24 +0900

    [level 2] Title: 타겟 넘버, Time: 4.82 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -7,34 +7,15 @@ class Solution {
     static int answer;
     public int solution(int[] numbers, int target) {
         n = numbers.length;
-        visited = new boolean[n];
-        //선택한거 -, 선택 안한거 +
-        arr = new int[n];
-        for(int i=0; i<n; i++) {
-            arr[i] = i+1;
-        }
-          
-        subSet(0,numbers,target);
-        
+        dfs(numbers, 0,0,target);
         return answer;
     }
-    static void subSet(int cnt, int[] numbers, int target) {
+    static void dfs(int[] numbers, int cnt, int sum, int target) {
         if(cnt == n) {
-            int c = cal(numbers);
-            if(c == target) answer++;
+            if(sum == target) answer++;
             return;
         }
-        visited[cnt] = true;
-        subSet(cnt+1, numbers, target);
-        visited[cnt] = false;
-        subSet(cnt+1, numbers, target);
-    }
-    static int cal(int[] numbers) { //선택된 것만 -을 해서 합을 계산
-        int sum =0;
-        for(int i=0; i<n; i++) {
-            if(visited[i]) sum = sum -(numbers[i]);
-            else sum = sum + numbers[i];
-        }
-        return sum;
+        dfs(numbers, cnt+1, sum+numbers[cnt],target);
+        dfs(numbers, cnt+1, sum-numbers[cnt],target);
     }
 }
