621085218e90f83442372e470324f5700ce9230f
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-12-19 11:02:48 +0900

    [Gold V] Title: 회장뽑기, Time: 128 ms, Memory: 16300 KB -BaekjoonHub

--- 백준/Gold/2660. 회장뽑기/회장뽑기.java
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.java
@@ -1,66 +1,62 @@
 import java.util.*;
-
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int memberNum = input.nextInt() + 1; // 회원 번호가 1번부터 시작하므로 memberNum 을 1개 늘려준다!
-        boolean[][] memberRelations = new boolean[memberNum][memberNum];
+	static StringTokenizer tokens;
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static int n;
+	static boolean [][] relation;
 
-        while (true) {
-            int i = input.nextInt();
-            int j = input.nextInt();
+	public static void main(String[] args) throws IOException {
+		n = Integer.parseInt(input.readLine()) + 1;
+		relation = new boolean[n][n];
+		while (true) {
+			tokens = new StringTokenizer(input.readLine());
+			int a = Integer.parseInt(tokens.nextToken());
+			int b = Integer.parseInt(tokens.nextToken());
+			if (a == -1 && b == -1) break;
+			relation[a][b] = true;
+			relation[b][a] = true;
+		}
+		bfs();
+	}
 
-            if (i == -1 && j == -1) break;
+	static void bfs() {
+		int[] res = new int[n]; // 후보자
+		int cnt = 0;
+		int minScore = Integer.MAX_VALUE;
 
-            memberRelations[i][j] = true;
-            memberRelations[j][i] = true;
-        }
+		for (int i = 1; i < n; i++) {
+			int[] score = new int[n];
+			Arrays.fill(score, -1);
+			Queue<Integer> que = new LinkedList<>();
+			que.add(i);
+			score[i] = 0;
 
-        solution(memberNum, memberRelations);
-    }
+			while (!que.isEmpty()) {
+				int current = que.poll();
 
-    private static void solution(int memberNum, boolean[][] memberRelations) {
-        int[] candidates = new int[memberNum]; // 후보를 저장할 배열
-        int candidateCount = 0; // 후보 수
-        int minScore = Integer.MAX_VALUE;
+				for (int j = 1; j < n; j++) {
+					if (relation[current][j] && score[j] == -1) {
+						que.add(j);
+						score[j] = score[current] + 1;
+					}
+				}
+			}
 
-        // 각 멤버 점수 계산
-        for (int i = 1; i < memberNum; i++) {
-            int[] scores = new int[memberNum];
-            Arrays.fill(scores, -1);
-            int[] nextMembers = new int[memberNum];
-            int queueSize = 0; // 큐의 크기를 관리
-            nextMembers[queueSize++] = i;
-            scores[i] = 0;
+			int maxScore = Arrays.stream(score).max().getAsInt();
+			if (maxScore > minScore) continue;
 
-            for (int front = 0; front < queueSize; front++) {
-                int member = nextMembers[front];
+			if (maxScore < minScore) {
+				minScore = maxScore;
+				cnt = 0;
+			}
+			res[cnt++] = i;
+		}
 
-                for (int j = 1; j < memberNum; j++) {
-                    if (memberRelations[member][j] && scores[j] < 0) {
-                        nextMembers[queueSize++] = j;
-                        scores[j] = scores[member] + 1;
-                    }
-                }
-            }
-
-            int score = Arrays.stream(scores).max().getAsInt();
-
-            if (score > minScore) continue;
-
-            if (score < minScore) {
-                minScore = score;
-                candidateCount = 0; // 후보 배열 초기화
-            }
-
-            candidates[candidateCount++] = i;
-        }
-
-        // 결과 출력
-        System.out.println(minScore + " " + candidateCount);
-        Arrays.sort(candidates, 0, candidateCount);
-        for (int i = 0; i < candidateCount; i++) {
-            System.out.print(candidates[i] + " ");
-        }
-    }
+		System.out.println(minScore + " " + cnt);
+		Arrays.sort(res, 0, cnt);
+		for (int i = 0; i < cnt; i++) {
+			System.out.print(res[i] + " ");
+		}
+	}
 }
