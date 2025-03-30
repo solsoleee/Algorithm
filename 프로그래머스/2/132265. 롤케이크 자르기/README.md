b4f468775b2f90eb5f94b07fe154216d479f31e4
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-03-30 17:23:42 +0900

    [level 2] Title: 롤케이크 자르기, Time: 206.79 ms, Memory: 168 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/README.md
+++ 프로그래머스/2/132265. 롤케이크 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 롤케이크 자르기 - 132265 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265) 
 
 ### 성능 요약
 
-메모리: 172 MB, 시간: 288.10 ms
+메모리: 168 MB, 시간: 206.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 31일 21:24:22
+2025년 03월 30일 17:23:40
 
 ### 문제 설명
 
--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,22 +1,24 @@
 import java.util.*;
+import java.io.*;
+
 class Solution {
     public int solution(int[] topping) {
-        int len = topping.length;
+        int answer = 0;
+        
         Set<Integer> set = new HashSet<>();
         Map<Integer, Integer> map = new HashMap<>();
-        int answer = 0;
-        for(int t :topping) {
+        
+        int len = topping.length;
+        //다 넣기
+        for(int t: topping) {
             map.put(t, map.getOrDefault(t,0)+1);
         }
-        
-        //하나씩 꺼내면서 탐색
         for(int i=0; i<len; i++) {
             set.add(topping[i]);
-            map.put(topping[i], map.get(topping[i])-1);
-            if(map.get(topping[i]) == 0) map.remove(topping[i]);
-            if(map.size() == set.size()) answer++;
+            map.put(topping[i], map.getOrDefault(topping[i],0)-1);
+            if(map.get(topping[i]) ==0 ) map.remove(topping[i]);
+            if(map.size() == set.size()) answer ++;
         }
-        
         
         return answer;
     }
