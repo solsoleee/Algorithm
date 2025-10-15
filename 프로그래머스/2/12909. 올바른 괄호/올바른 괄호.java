b92bd3510384d6ab6255b303d34da52ba8001c03
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-10-15 23:55:49 +0900

    [level 2] Title: 올바른 괄호, Time: 13.77 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,36 +1,17 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-
-        String st[] = s.split("");
-        
-        String left[] = new String[100000];
-        int n =0;
-        String pre="";
-        for(int i=0; i<st.length; i++) {
-            
-            if(st[i].equals("(")) {
-                left[n] = "(";
-                n++;
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(c);
+            } else { // ')'
+                if (stack.isEmpty()) return false;
+                stack.pop();
             }
-            else {
-                if(n==0) {
-                    answer = false;
-                    break;
-                }
-                if(left[n-1].equals("(")){
-                    n--;
-                }
-                
-            }
-            
         }
-        
-        if(n !=0) answer = false;
-        
-        return answer;
+        return stack.isEmpty();
     }
 }
