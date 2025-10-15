b7c33e6e4e247ba13424a89ecd828856cdc88ee4
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-10-15 22:36:40 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,24 +1,18 @@
 import java.util.*;
-import java.io.*;
-
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, Integer> map = new HashMap<>();
-        int n = clothes.length;
-        for(int i=0; i<n; i++) {
-            String k = clothes[i][1];
-            String v = clothes[i][0];
-            map.put(k, map.getOrDefault(k,0)+1);
-        }
+        int answer = 1;
         
-        int sum=1;
-        System.out.print(map.keySet());
-        for(String k: map.keySet()) {
-            sum = sum* (map.get(k) +1);
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0;i<clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1],0)+1);
         }
-        sum-=1;
-
-        //int answer = 0;
-        return sum;
+        for(String s:map.keySet()) {
+            answer *= map.get(s)+1 ;
+        }
+        answer--;
+        return answer;
     }
 }
+
+
