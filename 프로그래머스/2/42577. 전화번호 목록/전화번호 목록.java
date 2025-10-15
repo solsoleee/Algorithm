ffc73694cb0b73522d778f35153d2732ea89c9de
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-10-15 20:53:39 +0900

    [level 2] Title: 전화번호 목록, Time: 346.94 ms, Memory: 269 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,12 +1,20 @@
 import java.util.*;
-import java.io.*;
-
 class Solution {
     public boolean solution(String[] phone_book) {
-        List<String> list = new ArrayList<>(phone_book);
-        System.out.println(list);
-        
         boolean answer = true;
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<phone_book.length; i++) {
+            map.put(phone_book[i],i);
+        }
+        for(int i=0; i<map.size(); i++) {
+            for(int j=0; j<phone_book[i].length(); j++) {
+                if(map.containsKey(phone_book[i].substring(0, j))) {
+                    return false;
+                }
+                
+            }
+        }
+        
         return answer;
     }
 }
