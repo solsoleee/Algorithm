1a07d9dca4c359da3df3069ace484528d884af12
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-10-18 15:32:04 +0900

    [level 2] Title: 프로세스, Time: 2.26 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,32 +1,31 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<int[]> q = new ArrayDeque<>();                 // [index, priority]
-        PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder()); // 최대 우선순위
-
-        for (int i = 0; i < priorities.length; i++) {
-            q.offer(new int[]{i, priorities[i]});
-            max.offer(priorities[i]);
+        int answer = 0;
+        PriorityQueue <Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<int[]> que = new ArrayDeque<>();
+        for(int i=0; i<priorities.length; i++) {
+            que.offer(new int[]{i, priorities[i]});
+            pq.offer(priorities[i]);
         }
-
-        int printed = 0;
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int idx = cur[0];
-            int pri = cur[1];
-
-            // 현재 최고 우선순위와 비교
-            if (pri < max.peek()) {
-                // 아직 최고가 아니면 뒤로 보냄
-                q.offer(cur);
-            } else {
-                // 인쇄
-                printed++;
-                max.poll(); // 해당 우선순위 하나 소진
-                if (idx == location) return printed;
+        int rank = 1;
+        while(!que.isEmpty()) {
+            
+            int temp[] = que.poll();
+            //System.out.println(Arrays.toString(temp));
+            if(temp[1]==pq.peek()) { // 같으면 poll
+                pq.poll();
+                if(temp[0] == location) return rank;
+                rank++;
             }
+            else{ 
+                // 넣기
+                que.offer(new int[]{temp[0], temp[1]});
+            }
+            
+             
+            
         }
-        return -1; // 이론상 도달 X
+        return rank;
     }
 }
