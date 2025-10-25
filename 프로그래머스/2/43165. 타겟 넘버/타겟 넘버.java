88bf849f269f943fa2a9565953201609674b99e7
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-10-25 18:26:42 +0900

    [level 2] Title: 타겟 넘버, Time: 5.31 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,25 +1,24 @@
 import java.util.*;
+
 class Solution {
-    static int nums[];
-    static int target;
-    static int answer;
-    static int n;
+    int[] nums;
+    int target, n, answer;
+
     public int solution(int[] numbers, int targets) {
-        answer = 0;
         nums = numbers;
         target = targets;
         n = numbers.length;
-        dfs(0,0,0);
+        answer = 0;
+        dfs(0, 0);                 
         return answer;
     }
-    static void dfs(int cnt, int sum, int start) {
-        if(cnt == n) {
-            if(sum == target) answer++; //같으면
-            return; 
+
+    void dfs(int idx, int sum) {
+        if (idx == n) {
+            if (sum == target) answer++;
+            return;
         }
-        for(int i=start; i<n; i++) {
-            dfs(cnt+1, sum+nums[i], i+1);
-            dfs(cnt+1, sum-nums[i], i+1);
-        }
+        dfs(idx + 1, sum + nums[idx]);   // 현재 숫자에 +
+        dfs(idx + 1, sum - nums[idx]);   // 현재 숫자에 -
     }
 }
