43f1f0869bd3f3edf0e4eb5c754cf34a0ce4e1af
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-10-27 18:33:39 +0900

    [Gold V] Title: 토마토, Time: 596 ms, Memory: 105296 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,91 +1,74 @@
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static StringBuilder output = new StringBuilder();
-    private static StringTokenizer tokens;
-    private static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static boolean[][][] visited;
+    static int deltas[][] = { {0,0,1},{0,1,0},{0,-1,0},{0,0,-1},{1,0,0},{-1,0,0} };
+    static int box[][][];
+    static int H,M,N;
+    static Queue<int []> que;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    private static int n,m,h,count;
-    private static int box[][][];
-    private static boolean visited[][][];
-    private static int deltas[][] = {{1,0}, {-1,0},{0,1},{0,-1}};
-    private static int height[] = {1,-1};
-    private static Queue<int[]> que;
-    public static void main(String[] args) throws IOException {
+        M = Integer.parseInt(st.nextToken()); // 가로
+        N = Integer.parseInt(st.nextToken()); // 세로
+        H = Integer.parseInt(st.nextToken()); // 높이(상자 수)
+        box = new int[H][N][M];
+        visited = new boolean[H][N][M];
         que = new ArrayDeque<>();
-        tokens = new StringTokenizer(input.readLine());
-        m = Integer.parseInt(tokens.nextToken());
-        n = Integer.parseInt(tokens.nextToken());
-        h = Integer.parseInt(tokens.nextToken());
-        box = new int[h][n][m];
-        visited = new boolean[h][n][m];
-        for(int k=0; k<h; k++) {
-            for(int i=0; i<n; i++) {
-                tokens = new StringTokenizer(input.readLine());
-                for(int j=0; j<m; j++) {
-                    box[k][i][j] = Integer.parseInt(tokens.nextToken());
-                    if(box[k][i][j]==1) {
-                        que.offer(new int[] {k, i,j,0}); //익은것은 넣어줌
-                        visited[k][i][j] = true;
+        for(int h=0; h<H; h++) {
+            for(int i=0; i<N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for(int j=0; j<M; j++) {
+                    box[h][i][j] = Integer.parseInt(st.nextToken());
+                    if(box[h][i][j] == 1) {
+                        que.offer(new int[] {h, i, j, 0});
+                        visited[h][i][j] = true;
                     }
                 }
             }
         }
-        bfs(0);
-        if(check()) System.out.println(count);
+        int c = bfs();
+        if(tomato()) System.out.println(c);
         else System.out.println(-1);
-
     }
-
-    private static void bfs(int c) {
-        while(!que.isEmpty()) {
-            int t[] = que.poll();
-            int z = t[0];
-            int x = t[1];
-            int y = t[2];
-            int cnt = t[3];
-
-            count= Math.max(count, cnt);
-            //위, 뒤 일 때 익은 토마토
-
-            for(int i=0; i<2; i++){
-                int nz = z+height[i];
-                if(nz >=0 && nz<h) {
-                    if(!visited[nz][x][y] && box[nz][x][y]==0) {
-                        visited[nz][x][y]=true;
-                        box[nz][x][y]=1;
-                        que.offer(new int [] {nz,x,y,cnt+1});
-                    }
-                }
-            }
-            //상하좌우 익은 토마토
-            for(int d[]: deltas) {
-                int nx = x + d[0];
-                int ny = y + d[1];
-                if(nx >=0 && nx < n && ny>=0  && ny < m) {
-                    if(!visited[z][nx][ny] && box[z][nx][ny]==0) {
-                        visited[z][nx][ny] = true;
-                        box[z][nx][ny] = 1;
-                        que.add(new int[]{z, nx, ny,cnt+1});
-                    }
-                }
-            }
-        }
-    }
-
-    private static boolean check() {
-        for(int k=0; k<h; k++) {
-            for(int i=0; i<n; i++) {
-                for(int j=0; j<m; j++) {
-                    if(box[k][i][j]==0) {
-                        return false;
-                    }
+    //다 익었는지 확인
+    static boolean tomato() {
+        for(int h=0; h<H; h++) {
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<M; j++) {
+                    if(box[h][i][j] == 0) return false; //익지 않았음
                 }
             }
         }
         return true;
+    }
+
+    //익은 토마토
+    static int bfs() {
+        int res=0;
+        while(!que.isEmpty()) {
+            int temp[] = que.poll();
+            int cnt = temp[3];
+            res = cnt;
+            for(int d[]: deltas) {
+                int nz = temp[0]+d[0];
+                int nx = temp[1]+d[1];
+                int ny = temp[2]+d[2];
+                if(check(nz,nx,ny) && !visited[nz][nx][ny] ) {
+                    if(box[nz][nx][ny] == 0) {
+                        que.offer(new int [] {nz,nx,ny,cnt+1});
+                        visited[nz][nx][ny] = true;
+                        box[nz][nx][ny] = 1; //익음
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    static boolean check(int z, int x, int y) {
+        return (z>=0 && z<H && x>=0 && x<N && y>=0 && y<M);
     }
 }
