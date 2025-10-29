83b3811a31f6f4442e9b1c573cd5250339a9e4f3
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-10-29 21:00:43 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,7 +1,6 @@
--- 코드를 입력하세요
-SELECT b.ANIMAL_ID,	b.NAME
-from ANIMAL_INS as a
-right join animal_outs as b
-on a.animal_id=b.animal_id
-where a.animal_id is null
-order by b.animal_id, b.name
+-- 코드를 입력하세요 입양간 기록은 있는데 보호소에 들어온 기록이 없는 
+select ANIMAL_ID, name from animal_outs
+where animal_id not in (SELECT a.animal_id from animal_ins as a
+join animal_outs as b
+on a.animal_id = b.animal_id)
+-- 겹치는 것만
