2be5271cd0ed16efc60223c091f251559a9e084e
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-10-29 21:12:12 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,8 +1,7 @@
 -- 코드를 입력하세요
-SELECT a.NAME, a.DATETIME
-from ANIMAL_INS as a
-left join ANIMAL_OUTS as b
-on a.animal_id=b.animal_id
-where b.ANIMAL_ID is null
-order by a.DATETIME
+SELECT a.name, a.datetime from animal_ins as a
+left join animal_outs as b
+on a.animal_id = b.animal_id
+where b.animal_id is null
+order by a.datetime asc
 limit 3
