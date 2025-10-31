7507333157c3db78ccc2f8d0bcabaa2c2c5ad05b
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-10-31 15:47:42 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,6 +1,9 @@
 -- 코드를 입력하세요
-SELECT a.ANIMAL_ID, a.NAME
-FROM ANIMAL_INS AS a
-JOIN ANIMAL_OUTS AS b ON a.ANIMAL_ID = b.ANIMAL_ID
-ORDER BY DATEDIFF(b.DATETIME, a.DATETIME) DESC
-LIMIT 2;
+-- 보호 기간이 가장 길었던 동물
+-- ins에서 가장 먼저 들어오고 out에서 가장 나중에 나간
+-- out - ins 가 가장 큰거
+SELECT a.animal_id, a.name from ANIMAL_INS as a
+join ANIMAL_OUTS as b
+on a.animal_id = b.animal_id
+order by b.DATETIME - a.DATETIME desc
+limit 2
