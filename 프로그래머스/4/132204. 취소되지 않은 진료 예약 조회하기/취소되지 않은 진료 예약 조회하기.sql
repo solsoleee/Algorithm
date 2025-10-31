0a129b02a33f7133ba4145d1c3d3b7a66397bc38
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-10-31 16:09:17 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,12 +1,13 @@
-with appointment as (
-    select a.APNT_NO, c.MCDP_CD, c.DR_NAME, a.APNT_YMD, a.PT_NO
-    from APPOINTMENT as a
-    inner join DOCTOR as c on a.MDDR_ID=c.DR_ID
-    where c.mcdp_cd='CS' and a.APNT_CNCL_YN='N' and APNT_CNCL_YMD is null
-    and a.APNT_YMD like '2022-04-13%'
-)
-
-select a.APNT_NO, p.PT_NAME, p.PT_NO, a.MCDP_CD, a.DR_NAME, a.APNT_YMD
-from appointment as a
-inner join PATIENT as p on a.PT_NO=p.PT_NO
+-- 코드를 입력하세요
+SELECT a.APNT_NO, b.PT_NAME, b.PT_NO, a.MCDP_CD, c.DR_NAME, 
+a.APNT_YMD
+from APPOINTMENT as a
+join PATIENT as b
+on a.PT_NO = b.PT_NO
+join DOCTOR as c
+on a.MDDR_ID = c.DR_ID
+where date_format(a.APNT_YMD, '%Y-%m-%d')='2022-04-13' 
+and a.MCDP_CD='CS'
+and a.APNT_CNCL_YN='N'
+and a.APNT_CNCL_YMD is null
 order by APNT_YMD
